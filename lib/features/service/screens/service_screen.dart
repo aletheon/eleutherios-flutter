755d4705c48e190115f1cd49b6f7fe0fc064f43e
Rob <naitaubafarm@gmail.com>
2024-09-08 21:22:44 +1200

    changed

--- lib/features/service/screens/service_screen.dart
+++ lib/features/service/screens/service_screen.dart
@@ -18,6 +18,13 @@ import 'package:reddit_tutorial/theme/pallete.dart';
 import 'package:routemaster/routemaster.dart';
 import 'package:tuple/tuple.dart';
 
+// ***********************************************************************
+// ***********************************************************************
+// HERE ROB VALIDATE WHY shoppingCartUsers keeps being created so many times???
+// ***********************************************************************
+// ***********************************************************************
+// ***********************************************************************
+
 class ServiceScreen extends ConsumerStatefulWidget {
   final String serviceId;
   const ServiceScreen({super.key, required this.serviceId});
